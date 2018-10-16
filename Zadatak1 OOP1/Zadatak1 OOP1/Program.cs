579856e79ef19dedf2a9f666a785790582384f96
MarkoWWS <marko@wonderworldsolutions.com>
2018-10-16 22:07:54 +0200

    dodani system info

--- Zadatak1 OOP1/Zadatak1 OOP1/Program.cs
+++ Zadatak1 OOP1/Zadatak1 OOP1/Program.cs
@@ -5,12 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
+
 namespace Zadatak1_OOP1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //dodani argumenti u properties/debug
             Console.WriteLine("Aplikacija pokrenuta...\n");
             Console.WriteLine("Primljenih argumenata: {0}", args.Length);
             Console.WriteLine(@"{--------------------------------------\n---}"); //@ nam uzima doslovno sve sto pise u string nizu
@@ -58,16 +61,32 @@ namespace Zadatak1_OOP1
                     Console.WriteLine("Dostupan prostor korisniku(trenutnom): {0, 15} bytes", drive.AvailableFreeSpace);
                     Console.WriteLine("Cjelokupni dostupan prostor: {0, 15} bytes", drive.TotalFreeSpace);
                     Console.WriteLine("Totalni prostor na disku: {0, 15} bytes", drive.TotalSize);
+                    Console.WriteLine("Root folder: {0}", drive.RootDirectory);
                     Console.WriteLine("---------------------------------------------------------------\n");
 
                 }
 
             }
-            /*Doradite program tako da isp
-            isuje verziju operacijskog sustava, ime računala, korisničko ime, 
+            /*Doradite program tako da ispisuje verziju operacijskog sustava, ime računala, korisničko ime, 
             broj procesora, direktorij sustava, količinu memorije pridijeljene procesu u kilobajtima i 
             megabajtima, količinu dostupne virtualne memorije
             u kilobajtima i megabajtima.*/
+
+            Console.WriteLine("Operation System Information");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Operacijski sustav: {0}", Environment.OSVersion.VersionString);
+            Console.WriteLine("Ime racunala: {0}", Environment.MachineName);
+            Console.WriteLine("Korisnicko ime: {0}", Environment.UserName);
+            Console.WriteLine("Broj procesora: {0}", Environment.ProcessorCount);
+            Console.WriteLine("Direktorij sustava: {0}", Environment.SystemDirectory);
+            Console.WriteLine("Sustav 64bit?: {0}", Environment.Is64BitOperatingSystem);
+            //System.Diagnostics.PerformanceData.CounterData; //nest s tim treba??
+
+
+
+
+
+
 
 
 
