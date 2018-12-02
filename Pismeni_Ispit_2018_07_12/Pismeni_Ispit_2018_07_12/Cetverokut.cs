47dc8d88d99bdef9f0d26d7abc74b183542a51be
Stormcross <marko.lastavec@gmail.com>
2018-12-02 23:48:52 +0100

    cetverokut, kvadrat, pravokutnik

---
+++ Pismeni_Ispit_2018_07_12/Pismeni_Ispit_2018_07_12/Cetverokut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pismeni_Ispit_2018_07_12
+{
+    abstract class Cetverokut // 1.1
+    {
+        protected float a;
+        protected float b;
+
+        abstract public float IzracunajPovrsinu();
+        
+        abstract public string IspisiPodatke();
+        
+    }
+}
