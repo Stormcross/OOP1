e44cd4925826a229db59c5f2025b544bea89747e
Stormcross <marko.lastavec@gmail.com>
2018-12-02 23:41:49 +0100

    rijesen cetverokut, kvadrat, pravokutnik

---
+++ Pismeni_Ispit_2018_07_12/Pismeni_Ispit_2018_07_12/Pravokutnik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pismeni_Ispit_2018_07_12
+{
+    class Pravokutnik : Cetverokut //1.2
+    {
+        public Pravokutnik(float a, float b) //konstruktor
+        {
+            if (a > 0)
+            {
+                this.a = a;
+            }
+            else
+            {
+                throw new ArgumentException("Duljina stranice A ne može biti manja od 0");
+            }
+            if (b > 0)
+            {
+                this.b = b;
+            }
+            else
+            {
+                throw new ArgumentException("Duljina stranice B ne može biti manja od 0");
+            }
+        }
+
+        //moramo unjeti abstraktne tipve koje smo nasljedili iz cetverokut class
+        public override float IzracunajPovrsinu()
+        {
+            return a * b;
+        }
+
+        public override string IspisiPodatke()
+        {
+            string ispis = "Pravokutnik duljine stranica: " + a + " ," + b + "povrsine: " + IzracunajPovrsinu(a, b);
+            return ispis;
+        }
+    }
+}
---
+++ Pismeni_Ispit_2018_07_12/Pismeni_Ispit_2018_07_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// https://loomen.carnet.hr/pluginfile.php/1136454/mod_resource/content/0/2018.07.12.%20Pismeni%20ispit.pdf
+
+namespace Pismeni_Ispit_2018_07_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
