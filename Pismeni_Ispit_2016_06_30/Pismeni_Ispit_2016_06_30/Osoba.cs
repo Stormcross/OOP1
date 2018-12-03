f598c962816a092734c93eb6e6b224b4539b9d31
Stormcross <marko.lastavec@gmail.com>
2018-12-03 18:28:08 +0100

    osoba class

---
+++ Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Osoba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pismeni_Ispit_2016_06_30
+{
+    class Osoba:IEquatable<Osoba>
+    {
+        private string OIB; //1.1
+        private string Ime;
+        private string Prezime;
+
+        public Osoba(string OIB, string Ime, string Prezime) //1.2
+        {
+            if (provjeriOIB())
+            {
+                this.OIB = OIB;
+            }
+            else
+            {
+                throw new ArgumentException("Predani podatak sadrzava znakove koji nisu brojevi!"); //potrebno je upotrijebiti DataMisalignedException, prouciti
+            }
+            
+            this.Ime = Ime;
+            this.Prezime = Prezime;
+        }
+
+        protected bool provjeriOIB() //1.3
+        {
+            return OIB.All(char.IsDigit) && OIB.Length == 11; //drugaciji nacin nego preporuceno na ispitu
+        }
+
+        public string ispisiPodatke() //1.4
+        {
+            return String.Format("Osoba: {0,1} {1,1} Oib: {2}", Ime,Prezime, OIB);
+        }
+
+        public bool Equals(Osoba other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pismeni_Ispit_2016_06_30")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pismeni_Ispit_2016_06_30")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f15ba68-d3f5-4b1b-83ee-89b54a990205")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
