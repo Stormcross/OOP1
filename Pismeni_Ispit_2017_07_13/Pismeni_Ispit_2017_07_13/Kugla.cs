1723d9764da370e8bd275e3ccf2d1c461e2bc3c8
Stormcross <marko.lastavec@gmail.com>
2018-12-03 22:36:56 +0100

    Rijesen 1.#

---
+++ Pismeni_Ispit_2017_07_13/Pismeni_Ispit_2017_07_13/Kugla.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pismeni_Ispit_2017_07_13
+{
+    class Kugla
+    {
+        private double polumjer; //1.1
+
+        public double DovatiPolumjer() //1.2
+        {
+            return polumjer;
+        }
+
+        public void PostaviPolumjer(double polumjer) //1.3
+        {
+            this.polumjer = polumjer;
+        }
+
+        public double Polumjer //1.4
+        {
+            get { return this.polumjer; }
+            set { this.polumjer = value; }
+        }
+
+        public Kugla(double polumjer)
+        {
+            this.polumjer = polumjer;
+        } //1.5,6
+
+        public double Volumen()
+        {
+            return ((4 / 3) * Math.Pow(polumjer, 2) * Math.PI);
+        } //1.7
+
+        public double Oplosje()
+        {
+            return 4 * Math.Pow(polumjer, 2) * Math.PI;
+        } //1.8
+
+        public string ToString()
+        {
+            return String.Format("Polumjer: {0} Volumen: {1} Oplosje: {2}",polumjer,Volumen(),Oplosje());
+        } //1.9
+
+
+
+           
+
+
+
+
+
+        //Kraj klase
+    }
+}
---
+++ Pismeni_Ispit_2017_07_13/Pismeni_Ispit_2017_07_13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://loomen.carnet.hr/pluginfile.php/815839/mod_resource/content/0/2017.07.13.%20Pismeni%20ispit.pdf
+
+namespace Pismeni_Ispit_2017_07_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
