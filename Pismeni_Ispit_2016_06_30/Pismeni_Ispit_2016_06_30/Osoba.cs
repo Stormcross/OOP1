6360b0753bd0c14d3f3d15b091dade540707c375
Stormcross <marko.lastavec@gmail.com>
2018-12-12 21:22:45 +0100

    Rijesen 1.# dio ispita

--- Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Osoba.cs
+++ Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Osoba.cs
@@ -20,29 +20,57 @@ namespace Pismeni_Ispit_2016_06_30
             this.Prezime = Prezime;
         }
 
+        //Metoda zadaj
+        public void ZadajOIB(string OIB) //1.6
+        {
+            this.OIB = provjeriOIB(OIB);
+        }
+
+        //Metode vrati
+        public string VratiImeIPrezime() { return String.Format("{0}, {1}", Ime, Prezime); }
+        public string VratiIme() { return Ime; }
+        public string VratiPrezime() { return Prezime; }
+
+        //Ostale metode
         protected string provjeriOIB(string OIB) //1.3 Kako je to povezano s 1.6? pitati, provjeriti
         {
             bool test = false;
-
             if (OIB.All(char.IsDigit) != true)
             {
                 throw new DataMisalignedException("Predani podatak sadžava znakove koji nisu brojevi!");
-                
             }
-            else {
+            else
+            {
                 if (OIB.Length != 11)
                 {
                     throw new ArgumentOutOfRangeException("OIB", "Predani podatak sadrzava previse ili premalo znakova!");
                 }
                 else { test = true; }
             }
-
             if (test == true) return OIB;
             else return String.Format("Greska pri unosu OIB-a");
+        }
 
+        public string ispisiPodatke() //1.4
+        {
+            return String.Format("Osoba: {0,4} {1,4} Oib: {2}", Ime, Prezime, OIB);
+        }
+
+        public bool Equals(Osoba other) //1.5
+        {
+            //throw new NotImplementedException();
+            return Ime == other.VratiIme() &&
+                Prezime == other.VratiPrezime();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals((Osoba)obj);
         }
 
 
+
+        //Ovo dalje dolje je samo razmisljanje, ne ulazi u kod 
 
         //drugi nacin - prouciti jos kaj treba
         //try
@@ -81,19 +109,6 @@ namespace Pismeni_Ispit_2016_06_30
 
 
 
-        public string ispisiPodatke() //1.4
-        {
-            return String.Format("Osoba: {0,1} {1,1} Oib: {2}", Ime, Prezime, OIB);
-        }
 
-
-
-
-        public bool Equals(Osoba other) //1.5
-        {
-            //throw new NotImplementedException();
-            return Ime == other.Ime &&
-                Prezime == other.Prezime;
-        }
     }
 }
--- Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Program.cs
+++ Pismeni_Ispit_2016_06_30/Pismeni_Ispit_2016_06_30/Program.cs
@@ -13,15 +13,44 @@ namespace Pismeni_Ispit_2016_06_30
         static void Main(string[] args)
         {
             Osoba o1 = new Osoba("12345678901", "Mirko", "Miocic"); //1.6
+            Osoba o2 = new Osoba("22554477888", "Nikola", "Nikic");
+            Osoba o3 = new Osoba("99887766554", "Nikola", "Nikic");
+
+            //Ispis osoba
             Console.WriteLine(o1.ispisiPodatke());
-            Console.WriteLine("Podatak je dobro upisan"); //U slucaju da je sve u redu. //ostali slucajevi
+            Console.WriteLine(o2.ispisiPodatke());
+            Console.WriteLine(o3.ispisiPodatke());
+
+            //Novi OIB
+            UnesiNoviOIB(o1);
+
+            //1.6.2
+            Console.WriteLine(ProvjeraImePrezime(o2, o3));
+            //Zavrseno 1.## dio ispita
 
 
-            
+
+            Console.Read(); //dodano da sprijecimo gasenje programa
 
 
 
-            Console.Read(); //da sprijecimo gasenje programa
+            //Dodatne metode
+            void UnesiNoviOIB(Osoba osoba)//1.6.1
+            {
+                Console.WriteLine("\nUnesi novi OIB za osobu: {0}", osoba.VratiImeIPrezime());
+                string OIB;
+                Console.Write("Novi OIB: "); OIB = Console.ReadLine();
+                osoba.ZadajOIB(OIB);
+                Console.WriteLine("Podatak je dobro upisan...\n"); //U slucaju da je sve u redu. 
+            }
+
+            string ProvjeraImePrezime(object IP1, object IP2)
+            {
+                return String.Format("Osobe 2 i 3 imaju isto ime i prezime: {0}", IP1.Equals(IP2));
+            }
+
+
+
         }
 
 
