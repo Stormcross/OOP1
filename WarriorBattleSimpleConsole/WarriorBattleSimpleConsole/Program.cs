42308481fd3a9940a49420d0fc3806db33330071
Stormcross <marko.lastavec@gmail.com>
2019-01-15 16:32:17 +0100

    Warrior igra verzija 1.0 - nema input samo slugfest

---
+++ WarriorBattleSimpleConsole/WarriorBattleSimpleConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarriorBattleSimpleConsole.Logika;
+
+namespace WarriorBattleSimpleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior ivo = new Warrior("Ivo", 500, 100, 50);
+            Warrior mirko = new Warrior("Mirko", 500, 100, 50);
+
+            Battle.StartFight(ivo, mirko);
+
+            Console.ReadLine();
+        }
+    }
+}
